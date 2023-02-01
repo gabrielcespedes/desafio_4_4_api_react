6f8c75b2d726376059c64aa69549041c307f2994
Gabriel Céspedes <gabrielcespedesalarc@gmail.com>
2023-01-31 22:15:49 -0300

    version estable

--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const MiApi = () => {    
     const url = 'https://pokeapi.co/api/v2/pokemon/65/';
     const [info, setInfo] = useState();   
-    const [input_filter, setInput_filter] = useState();
+    const [input_filter, setInput_filter] = useState('');
 
     const fetchApi = async () => {
         const response = await fetch(url);
@@ -22,7 +22,7 @@ const MiApi = () => {
     
     return(<div>
             <h1>hola</h1>
-            <h1>Movimientos de {info.forms[0].name}</h1>
+            <h1>Movimientos de {info.forms[0].name}</h1>   
             <input placeholder="Busca un movimiento" onChange={handleInputFilter}></input>
             <ul>
                 {
@@ -35,7 +35,7 @@ const MiApi = () => {
                             return '';
                         }
                     }).map((element, index) => {return(<li key={index}>{element.move.name}</li>)})}
-            </ul>                     
+            </ul>                        
         </div>
     )
 }
