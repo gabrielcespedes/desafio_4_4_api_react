d127d89dfe9fde85a0ffc94a6cb3aae3ba5f72b9
Gabriel Céspedes <gabrielcespedesalarc@gmail.com>
2023-02-01 18:46:16 -0300

    version final_

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import MiApi from "./components/MiApi";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -19,31 +19,54 @@ const MiApi = () => {
     const handleInputFilter = (e) => {
         setInput_filter(e.target.value);
     }
+
+    const name = info.forms[0].name[0].toUpperCase() + info.forms[0].name.slice(1);
     
     return(<div>
-            <h1>hola</h1>
-            <h1>Movimientos de {info.forms[0].name}</h1>       
-            <input placeholder="Busca un movimiento" onChange={handleInputFilter}></input>
-            <ul>
-                {
-                    info.moves.filter((elemento) => {
-                        if (input_filter === '') {
-                            return elemento;
-                        } else if (elemento.move.name.includes(input_filter)) {
-                            return elemento;
-                        } else {
-                            return '';
-                        }
-                    }).sort(function(a,b) {
-                        if (a.move.name > b.move.name) {
-                            return 1;
-                        }
-                        if (a.move.name < b.move.name) {
-                            return -1;
-                        }
-                        return 0;
-                    }).map((element, index) => {return(<li key={index}>{element.move.name}</li>)})}
-            </ul>                            
+            <div className="container-fluid">
+                <h1 className="text-center">{name}</h1>
+                <h2>Estadísticas de {name}</h2>
+                <table className="table">
+                    <thead>
+                        <tr>
+                        <th>Nombre</th>
+                            <th>Puntaje Base</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {info.stats.map((element, index) => {
+                        return(<tr>
+                            <th key={index}>{element.stat.name}</th> 
+                            <th key={index}>{element.base_stat}</th>
+                        </tr>
+                        )})} 
+                    </tbody>                              
+                </table>
+            </div>
+            <div className="container">
+                <h2>Movimientos de {name}</h2>       
+                <input className="form-control text-center bg-dark text-white" placeholder="Busca un movimiento" onChange={handleInputFilter}></input>
+                <ul className="list-group bg-muted">
+                    {
+                        info.moves.filter((elemento) => {
+                            if (input_filter === '') {
+                                return elemento;
+                            } else if (elemento.move.name.toLocaleLowerCase().includes(input_filter.toLocaleLowerCase())) {
+                                return elemento;
+                            } else {
+                                return '';
+                            }
+                        }).sort(function(a,b) {
+                            if (a.move.name > b.move.name) {
+                                return 1;
+                            }
+                            if (a.move.name < b.move.name) {
+                                return -1;
+                            }
+                            return 0;
+                        }).map((element, index) => {return(<li className="list-group-item" key={index}>{element.move.name}</li>)})}
+                </ul>         
+            </div>                                               
         </div>
     )
 }
